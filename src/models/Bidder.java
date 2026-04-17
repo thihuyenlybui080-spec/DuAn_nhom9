89c092ec8581b0975aef55e92b628e4760c9210c
NhatBui12-web <vangd408@gmail.com>
2026-04-17 15:18:27 +0700

    Update Bidder.java

--- src/models/Bidder.java
+++ src/models/Bidder.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class Bidder extends User {
     private List<BidTransaction> history;
 
-    public Bidder(String name, String id, String password, String email, String fullName){
-        super(name, id, password, email, fullName);
+    public Bidder(String id, String name, String password, String email, String fullName){
+        super(id, name, password, email, fullName);
         this.history  = new ArrayList<>();
     }
 
