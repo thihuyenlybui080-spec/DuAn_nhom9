5880ac3056c3c279f6ad3712f944a01388d0dc60
NhatBui12-web <vangd408@gmail.com>
2026-04-17 15:19:04 +0700

    Update Admin.java

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -5,8 +5,8 @@ import base.User;
 import exceptions.AuthenticationException;
 
 public class Admin extends User {
-    public Admin(String userName, String id, String password, String email, String fullName) {
-        super(userName, id, password, email, fullName);
+    public Admin(String id, String userName, String password, String email, String fullName) {
+        super(id, userName, password, email, fullName);
     }
 
     public void monitorAuction(AuctionManager auctionManager) {
