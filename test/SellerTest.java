5dd5f9a8f9cbe66c927542b606e276ced38c1fa3
duongtl-8989 <duongtl0733@gmail.com>
2026-04-19 21:49:49 +0700

    Add files via upload

---
+++ test/SellerTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SellerTest {
+    private Seller seller;
+
+    @BeforeEach
+    void setUp() {
+        // Khởi tạo đối tượng Admin trước mỗi bài test
+        seller = new Seller("duong", "AD01", "123456", "admin@gmail.com", "Nguyen Van Admin");
+    }
+
+    @Test
+    void testLogInSuccess() {
+        // Kiểm tra đăng nhập đúng
+        assertDoesNotThrow(() -> {
+
+            seller.logIn("duong", "123456");
+        });
+    }
+
+    @Test
+    void testLogInFail() {
+        // Kiểm tra đăng nhập sai username hoặc password sẽ ném ra Exception
+        assertThrows(AuthenticationException.class, () -> {
+            seller.logIn("admin1", "wrong_pass");
+        });
+    }}
+
