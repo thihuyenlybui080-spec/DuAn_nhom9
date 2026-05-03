15dfa42f9522d3ef79066f9ffb7cd1616169825a
NhatBui12-web <vangd408@gmail.com>
2026-05-03 16:20:17 +0700

    Update ItemFactory.java
    xóa currentPrice

--- src/factories/ItemFactory.java
+++ src/factories/ItemFactory.java
@@ -5,6 +5,6 @@ import base.Item;
 import java.time.LocalDateTime;
 
 public abstract class ItemFactory {
-    public abstract Item createItem(String id, String itemName, String description, double startingPrice, double currentPrice, LocalDateTime startingTime, LocalDateTime
+    public abstract Item createItem(String id, String itemName, String description, double startingPrice,  LocalDateTime startingTime, LocalDateTime
             endTime);
 }
