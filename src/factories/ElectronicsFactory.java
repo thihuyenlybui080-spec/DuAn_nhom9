1d83f6095191029e284c0a4cbacd97b0ee45797c
NhatBui12-web <vangd408@gmail.com>
2026-05-03 16:19:24 +0700

    Update ElectronicsFactory.java
    xóa currentPrice

--- src/factories/ElectronicsFactory.java
+++ src/factories/ElectronicsFactory.java
@@ -6,8 +6,8 @@ import models.item.Electronics;
 import java.time.LocalDateTime;
 public class ElectronicsFactory extends ItemFactory{
     @Override
-    public Item createItem(String id, String itemName, String description, double startingPrice, double currentPrice, LocalDateTime startingTime, LocalDateTime
+    public Item createItem(String id, String itemName, String description, double startingPrice, LocalDateTime startingTime, LocalDateTime
             endTime){
-        return new Electronics(id, itemName,description,startingPrice, currentPrice, startingTime, endTime);
+        return new Electronics(id, itemName,description,startingPrice, startingTime, endTime);
     }
 }
