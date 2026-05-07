a308eb3616d1a5b095b96862452afedcd0f78752
thihuyenlybui080-spec <thihuyenlybui080@gmail.com>
2026-05-07 16:56:31 +0700

    Delete AuctionSystem_final directory

--- AuctionSystem_final/src/main/java/org/example/loginregister/common/observer/Subject.java
+++
@@ -1,7 +0,0 @@
-package org.example.loginregister.common.observer;
-
-public interface Subject {
-    void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}
