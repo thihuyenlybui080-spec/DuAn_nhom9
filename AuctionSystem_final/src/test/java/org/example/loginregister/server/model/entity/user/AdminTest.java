83512678aa4f560cccab50ce0923364548257613
duongtl-8989 <duongtl0733@gmail.com>
2026-05-08 00:14:14 +0700

    test

---
+++ AuctionSystem_final/src/test/java/org/example/loginregister/server/model/entity/user/AdminTest.java
@@ -0,0 +1,32 @@
+package org.example.loginregister.server.model.entity.user;
+import org.example.loginregister.common.exception.AuthenticationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AdminTest {
+    private Admin admin;
+
+    @BeforeEach
+    void setUp() {
+        // Khởi tạo đối tượng Admin trước mỗi bài test
+        admin = new Admin("AD01", "duong", "123456", "admin@gmail.com", "Nguyen Van Admin");
+    }
+
+    @Test
+    void testLogInSuccess() {
+        // Kiểm tra đăng nhập đúng
+        assertDoesNotThrow(() -> {
+
+            admin.logIn("duong", "123456");
+        });
+    }
+
+    @Test
+    void testLogInFail() {
+        // Kiểm tra đăng nhập sai username hoặc password sẽ ném ra Exception
+        assertThrows(AuthenticationException.class, () -> {
+            admin.logIn("duong", "1233");
+        });
+    }}
