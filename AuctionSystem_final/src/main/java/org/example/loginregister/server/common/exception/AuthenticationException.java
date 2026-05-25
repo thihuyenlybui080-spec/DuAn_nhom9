baee4b645237945bfd11b000df0d3be19374190b
thuthuy25022024 <25022024@vnu.edu.vn>
2026-05-25 15:59:40 +0700

    Delete AuctionSystem_final directory

--- AuctionSystem_final/src/main/java/org/example/loginregister/server/common/exception/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package org.example.loginregister.server.common.exception;
-
-public class AuthenticationException extends Exception {
-    public AuthenticationException(String msg){
-        super(msg);
-    }
-}
-
--- AuctionSystem_final/src/main/java/org/example/loginregister/server/common/observer/Observer.java
+++
@@ -1,6 +0,0 @@
-package org.example.loginregister.server.common.observer;
-
-public interface Observer {
-    public abstract void update(String auctionId, double newPrice, String highestBidder);
-}
-
